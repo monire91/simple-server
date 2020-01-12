7856cfce610d4ec9177f6dac804ad7db1c8ee857
monireh <aglida1370@gmail.com>
2020-01-13 00:03:01 +0330

    first commit

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ app.get('/', (req,res)=>{
     res.send('home pageeee');
 });
 
-const PORT = process.env.PORT ||5000;
+const PORT = process.env.PORT ||80;
 
 app.listen(PORT, ()=>{
     console.log(`Server started on port ${PORT}`);
